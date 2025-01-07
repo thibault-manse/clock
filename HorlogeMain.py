30083bef1ebd2ebca4102bae87de1cca9750b03c
thibault-manse <thibault.manse@laplateforme.io>
2025-01-07 09:51:58 +0100

    Choix de heure préconfiguré ou non

--- HorlogeMain.py
+++ HorlogeMain.py
@@ -1,5 +1,15 @@
 import time #sert a utiliser time.sleep
 
+def heure_voulu(): #Choix heure auto ou configurer
+    check = 0
+    while check == 0:
+        val = str(input("Voulez vous une heure prés définie ? (Y ou N) : "))
+        if val == "Y" or val == "y" or val == "N" or val == "n":
+            check = 1
+        else :
+            print("Valeur incorrect")
+    return val
+
 def afficher_heure(): #Réglage de l'heure
     check = 0
     while check == 0:
@@ -13,36 +23,46 @@ def afficher_heure(): #Réglage de l'heure
     horaire = (val1, val2, val3)
     return horaire
 
-h, m, s = afficher_heure()
+veref = heure_voulu()
 
-#Afficher 01:00:00 et non 1:0:0 (transformer en str)
-val_h = str(0)
-val_m = str(0)
-val_s = str(0)
+if veref == "y" or veref == "Y": #Horloge préconfiguré
+    while True:
+        time_live = time.strftime("%H:%M:%S")
+        print(f"{time_live}", end="\r")
+        time.sleep(1)
 
-heure = "%02d" % h
-minute = "%02d" % m
-seconde = "%02d" % s
-#-----------------------------
 
-while True: #Boucle affichage de l'heure
-    print("{H}:{M}:{S}".format(H = heure, M = minute, S = seconde), end="\r") #affiche l'heure sur une seule ligne
-    s += 1
-    seconde = chr(ord(val_s)+s) # +1 a la valeur ascii
+else : #Horloge configurer
+    h, m, s = afficher_heure()
+
+    #Afficher 01:00:00 et non 1:0:0 (transformer en str)
+    val_h = str(0)
+    val_m = str(0)
+    val_s = str(0)
+
+    heure = "%02d" % h
+    minute = "%02d" % m
     seconde = "%02d" % s
-    if s == 60:
-        m += 1
-        s = 0
-        minute = chr(ord(val_m)+m)
-        minute = "%02d" % m
-        seconde = "%02d" % 0
-        if m == 60:
-            h += 1
-            m = 0
-            heure = chr(ord(val_h)+h)
-            heure = "%02d" % h
-            minute = "%02d" % 0
-            if h == 24:
-                h = 0
-                heure = "%02d" % 0
-    time.sleep(1)
+    #-----------------------------
+
+    while True: #Boucle affichage de l'heure
+        print("{H}:{M}:{S}".format(H = heure, M = minute, S = seconde), end="\r") #affiche l'heure sur une seule ligne
+        s += 1
+        seconde = chr(ord(val_s)+s) # +1 a la valeur ascii
+        seconde = "%02d" % s
+        if s == 60:
+            m += 1
+            s = 0
+            minute = chr(ord(val_m)+m)
+            minute = "%02d" % m
+            seconde = "%02d" % 0
+            if m == 60:
+                h += 1
+                m = 0
+                heure = chr(ord(val_h)+h)
+                heure = "%02d" % h
+                minute = "%02d" % 0
+                if h == 24:
+                    h = 0
+                    heure = "%02d" % 0
+        time.sleep(1)
